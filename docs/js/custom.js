44dcab622e248b764af39bd9567d01e2e170d689
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2021-09-28 15:48:37 -0500

    updated CSS and Javascript

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -3,10 +3,18 @@
 
 $(".main_wrapper header .inner .language_nav ul li:nth-child(1) .flag_box").on('click', function(){
     $('.heading .desc .language_changer:nth-child(1) ').css({ "display": "none" });
-    $('.heading .desc .language_changer:nth-child(2) ').css({ "display": "block" }); 
-     $('.heading .desc .language_changer:nth-child(3) ').css({ "display": "none" });
+    $('.heading .desc .language_changer:nth-child(2) ').css({ "display": "none" }); 
+     $('.heading .desc .language_changer:nth-child(3) ').css({ "display": "block" });
       $('.heading .desc .language_changer:nth-child(4) ').css({ "display": "none" });
 });
+
+$(".main_wrapper header .inner .language_nav ul li:nth-child(2) .flag_box").on('click', function(){
+    $('.heading .desc .language_changer:nth-child(1) ').css({ "display": "none" });
+    $('.heading .desc .language_changer:nth-child(2) ').css({ "display": "block" }); 
+    $('.heading .desc .language_changer:nth-child(3) ').css({ "display": "none" });
+    $('.heading .desc .language_changer:nth-child(4) ').css({ "display": "none" });
+});
+
 
 $(".main_wrapper header .inner .language_nav ul li:nth-child(3) .flag_box").on('click', function(){
     $('.heading .desc .language_changer:nth-child(1) ').css({ "display": "block" });
@@ -15,9 +23,10 @@ $(".main_wrapper header .inner .language_nav ul li:nth-child(3) .flag_box").on('
     $('.heading .desc .language_changer:nth-child(4) ').css({ "display": "none" });
 });
 
+
 $(".main_wrapper header .inner .language_nav ul li:nth-child(4) .flag_box").on('click', function(){
     $('.heading .desc .language_changer:nth-child(1) ').css({ "display": "none" });
     $('.heading .desc .language_changer:nth-child(2) ').css({ "display": "none" }); 
-     $('.heading .desc .language_changer:nth-child(3) ').css({ "display": "block" });
-      $('.heading .desc .language_changer:nth-child(4) ').css({ "display": "none" });
+     $('.heading .desc .language_changer:nth-child(3) ').css({ "display": "none" });
+      $('.heading .desc .language_changer:nth-child(4) ').css({ "display": "block" });
 });
